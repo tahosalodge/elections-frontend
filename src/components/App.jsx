e243fd65f7ed4d649c7a8fafe153db6dd9bf1718
Kevin McKernan <kevin@mckernan.in>
2017-11-14 23:20:54 -0700

    Add token check to App, add page wrapper style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,12 @@ import propTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
 import Header from './Header';
 import Home from './Home';
 import UnitInformation from './Forms/UnitInformation';
+import Unit from './Unit';
 import ElectionForm from './Forms/Election';
 import Candidate from './Forms/Candidate';
 import Nomination from './Forms/Nomination';
@@ -14,33 +17,57 @@ import ElectionList from './ElectionList';
 import Election from './Election';
 import Register from './Forms/Register';
 import Login from './Forms/Login';
+import { loginVerifyRequest } from '../redux/modules/login';
 
-const App = ({ history, capability }) => (
-  <ConnectedRouter history={history}>
-    <div>
-      <Header menu={capability} />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/unit-information" component={UnitInformation} />
-        <Route path="/request-election" component={ElectionForm} />
-        <Route path="/candidate" component={Candidate} />
-        <Route path="/nomination" component={Nomination} />
-        <Route path="/election" component={Election} />
-        <Route path="/election-list" component={ElectionList} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-      </Switch>
-    </div>
-  </ConnectedRouter>
-);
+const Page = styled.div`
+  max-width: 1200px;
+  width: 90%;
+  padding: 2em 5em;
+  background: white;
+  margin: 0 auto;
+  min-height: 60vh;
+  margin-top: 2em;
+`;
+
+class App extends React.PureComponent {
+  componentWillMount() {
+    this.props.loginVerifyRequest();
+  }
+  render() {
+    const { history, capability } = this.props;
+    return (
+      <ConnectedRouter history={history}>
+        <div>
+          <Header menu={capability} />
+          <Page>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/units/new" component={UnitInformation} />
+              <Route path="/units/:id" component={Unit} />
+              <Route path="/units/" component={Unit} />
+              <Route path="/request-election" component={ElectionForm} />
+              <Route path="/candidate" component={Candidate} />
+              <Route path="/nomination" component={Nomination} />
+              <Route path="/election" component={Election} />
+              <Route path="/election-list" component={ElectionList} />
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </Page>
+        </div>
+      </ConnectedRouter>
+    );
+  }
+}
 
 App.propTypes = {
   history: propTypes.object.isRequired, // eslint-disable-line
   capability: propTypes.string.isRequired,
+  loginVerifyRequest: propTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   capability: state.login.capability,
 });
 
-export default connect(mapStateToProps, null)(App);
+export default connect(mapStateToProps, { loginVerifyRequest })(App);
