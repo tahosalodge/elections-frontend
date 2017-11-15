ef872f94b65efd278dcca4422e5bee7a52dece24
Kevin McKernan <kevin@mckernan.in>
2017-11-14 23:19:02 -0700

    Mappable headers for tables, styles

--- src/components/Table/Row.jsx
+++ src/components/Table/Row.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const Row = ({ data }) => <tr>{Object.keys(data).map(item => <td>{data[item]}</td>)}</tr>;
+const Row = ({ headers, data }) => {
+  const processedData = headers.map((header) => {
+    if (header.field === 'actions') {
+      return header.component;
+    }
+    return data[header.field];
+  });
+  return <tr>{processedData.map(item => <td key={item}>{item}</td>)}</tr>;
+};
 Row.propTypes = {
+  headers: propTypes.arrayOf(propTypes.object).isRequired,
   data: propTypes.object.isRequired,
 };
 export default Row;
--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import styled from 'styled-components';
+import { colors } from '../../constants';
 import Row from './Row';
 
+const StyledTable = styled.table`
+  border: 1px solid #dedede;
+  width: 100%;
+  border-collapse: collapse;
+
+  th {
+    font-size: 1.2em;
+    text-align: left;
+  }
+
+  th,
+  td {
+    border: 1px solid #dedede;
+    padding: 0.6rem;
+  }
+
+  button {
+    background: ${colors.red};
+    padding: 0.5em 1em;
+    color: white;
+    font-size: 0.8em;
+    border: 0;
+    border-radius: 0;
+  }
+`;
+
 const Table = ({ headers, data }) => (
-  <table>
+  <StyledTable>
     <thead>
-      <tr>{headers.map(value => <th>{value}</th>)}</tr>
+      <tr>{headers.map(value => <th key={value.title}>{value.title}</th>)}</tr>
     </thead>
-    <tbody>
-      {data.map(item => <Row data={item} />)}
-    </tbody>
-  </table>
+    <tbody>{data.map(item => <Row key={item._id} headers={headers} data={item} />)}</tbody>
+  </StyledTable>
 );
 
 Table.propTypes = {
-  headers: propTypes.array.isRequired,
-  data: propTypes.array.isRequired,
+  headers: propTypes.arrayOf(propTypes.object).isRequired,
+  data: propTypes.arrayOf(propTypes.object).isRequired,
 };
 
 export default Table;
