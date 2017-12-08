45f138fe8af6c92d3f497607b8535ff448e0d991
Kevin McKernan <kevin@mckernan.in>
2017-12-07 17:49:44 -0700

    Use date-fns for date formatting

--- src/components/Table/Row.jsx
+++ src/components/Table/Row.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { format } from 'date-fns';
 
 const Row = ({ headers, data, parentKey }) => {
   const cols = headers.map((header) => {
     if (header.field === 'actions') {
       return <td key={`${parentKey}-${header.field}`}>{header.component}</td>;
+    }
+    if (header.field === 'date') {
+      return (
+        <td key={`${parentKey}-${header.field}`}>
+          {format(data[header.field], 'dddd, MMMM DD YYYY hh:mm A')}
+        </td>
+      );
     }
     return <td key={`${parentKey}-${header.field}`}>{data[header.field]}</td>;
   });
