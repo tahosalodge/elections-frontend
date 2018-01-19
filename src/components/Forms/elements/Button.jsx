989885995f9dcea6ff7f4b94ffce6573aae5a1e6
Kevin McKernan <kevin@mckernan.in>
2018-01-19 12:29:46 -0700

    button styles

--- src/components/Forms/elements/Button.jsx
+++ src/components/Forms/elements/Button.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import styled from 'styled-components';
+import { colors } from 'constants/values';
 
+const ButtonStyles = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1em;
+
+  button {
+    background: ${colors.red};
+    border: 0;
+    color: white;
+    font-size: 16px;
+    padding: 0.5em 1em;
+    width: 100%;
+  }
+`;
 const Button = ({ type, text, disabled }) => (
-  <div>
+  <ButtonStyles>
     <button type={type} disabled={disabled}>
       {text}
     </button>
-  </div>
+  </ButtonStyles>
 );
 
 Button.propTypes = {
