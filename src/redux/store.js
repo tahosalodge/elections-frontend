c64ebbc25380375dc7206c1dfe06b9fa622cc9b6
Kevin McKernan <kevin@mckernan.in>
2018-01-25 20:38:29 -0700

    Sentry redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,16 +2,18 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import { routerMiddleware } from 'react-router-redux';
-
+import ravenMiddleware from 'raven-for-redux';
 import rootReducer from './reducers';
 import rootSaga from './sagas';
+
+const { Raven } = window;
 
 export default function (history) {
   const sagaMiddleware = createSagaMiddleware();
   const router = routerMiddleware(history);
   const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(sagaMiddleware, router)),
+    composeWithDevTools(applyMiddleware(ravenMiddleware(Raven), sagaMiddleware, router)),
   );
   sagaMiddleware.run(rootSaga);
 
