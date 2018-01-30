7a461f699ea42b32575e25694e60a85a199af319
Kevin McKernan <kevin@mckernan.in>
2018-01-30 01:02:42 -0700

    Add import to UI

--- src/components/Admin/UnitImport.jsx
+++ src/components/Admin/UnitImport.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { addToast } from 'redux/state/toasts';
 
 class UnitImport extends React.Component {
-  state = { unitData: {} };
+  state = {
+    unitData: {},
+    unitId: '',
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const token = localStorage.getItem('electionToken');
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/admin/import-unit/${this.state.unitId}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      this.props.addToast(`Unit ${this.state.unitId} added successfully.`);
+    } catch (e) {
+      this.props.addToast(`There was an error importing ${this.state.unitId}.`);
+    }
+  };
+
+  handleChange = (event) => {
+    this.setState({ unitId: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Unit Id</label>
+          <br />
+          <br />
+          <input type="text" name="unitId" onChange={this.handleChange} />
+          <br />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default connect(null, { addToast })(UnitImport);
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -14,6 +14,7 @@ import Register from 'components/Forms/Register';
 import Login from 'components/Forms/Login';
 import Help from 'components/Help';
 import Logout from 'components/Logout';
+import UnitImport from 'components/Admin/UnitImport';
 
 const Routes = () => (
   <Switch>
@@ -33,6 +34,7 @@ const Routes = () => (
     <Route path="/help" component={Help} />
     <Route path="/login" component={Login} />
     <Route path="/logout" component={Logout} />
+    <Route path="/admin/import" component={UnitImport} />
   </Switch>
 );
 
