aec77632e28d7eac0b332280703e667a128380d7
Kevin McKernan <kevin@mckernan.in>
2018-02-04 10:47:57 -0700

    nomination and candidate redux

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -4,7 +4,17 @@ import { userSaga } from 'redux/state/user';
 import { unitSaga } from 'redux/state/unit';
 import { electionSaga } from 'redux/state/election';
 import { toastsSaga } from 'redux/state/toasts';
+import { nominationSaga } from 'redux/state/nomination';
+import { candidateSaga } from 'redux/state/candidate';
 
 export default function* RootSaga() {
-  yield all([registerSaga(), userSaga(), unitSaga(), electionSaga(), toastsSaga()]);
+  yield all([
+    registerSaga(),
+    userSaga(),
+    unitSaga(),
+    electionSaga(),
+    toastsSaga(),
+    nominationSaga(),
+    candidateSaga(),
+  ]);
 }
