f3dc277f61846565a563c098f39982b6ee44e6b9
Kevin McKernan <kevin@mckernan.in>
2018-02-15 13:41:35 -0700

    Conditionally display form, only if results have not been entered

--- src/components/Election/pages/Report.jsx
+++ src/components/Election/pages/Report.jsx
@@ -1,11 +1,28 @@
 import React, { Fragment as F } from 'react';
 import ElectionReport from 'components/Forms/ElectionReport';
+import { arrayOfCandidates } from 'shapes/candidate';
+import { election as matchShape } from 'shapes/match';
+import electionShape from 'shapes/election';
 
 const Report = ({ match, election, candidates }) => (
   <F>
     <h1>Election Report</h1>
-    <ElectionReport match={match} election={election} candidates={candidates} />
+    {election.status === 'Results Entered' && (
+      <p>
+        Results may not be modified once submitted. Please contact elections@tahosalodge.org if you
+        need to make any changes.
+      </p>
+    )}
+    {election.status !== 'Results Entered' && (
+      <ElectionReport match={match} election={election} candidates={candidates} />
+    )}
   </F>
 );
+
+Report.propTypes = {
+  match: matchShape.isRequired,
+  election: electionShape.isRequired,
+  candidates: arrayOfCandidates.isRequired,
+};
 
 export default Report;
